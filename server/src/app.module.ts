038d69cdf837ff7fdfb3746314d2265c9439cb87
Klementeva Katya <87204318+easy-breezy-lemon-squeezy@users.noreply.github.com>
2022-12-17 23:42:06 +0300

    9. Загрузка файлов

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,23 +4,25 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { getMongoConfig } from './config/mongo.config'
 import { TypegooseModule } from 'nestjs-typegoose'
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { GenreModule } from './genre/genre.module';
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
+import { GenreModule } from './genre/genre.module'
+import { FileModule } from './file/file.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypegooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getMongoConfig,
-    }),
-    AuthModule,
-    UserModule,
-    GenreModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
+		AuthModule,
+		UserModule,
+		GenreModule,
+		FileModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
