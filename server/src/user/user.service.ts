917c082b5e6674584ce7df598bb9a0cc2da6aef7
Klementeva Katya <87204318+easy-breezy-lemon-squeezy@users.noreply.github.com>
2022-12-19 15:35:09 +0300

    14. Избранное

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { genSalt, hash } from 'bcryptjs'
+import { Types } from 'mongoose'
 import { InjectModel } from 'nestjs-typegoose'
 import { UpdateDto } from './dto/update.dto'
 import { UserModel } from './user.model'
@@ -65,5 +66,29 @@ export class UserService {
 
 	async delete(id: string) {
 		return this.UserModel.findByIdAndDelete(id).exec()
+	}
+
+	async toggleFavorite(movieId: Types.ObjectId, user: UserModel) {
+		const { favorites, _id } = user
+
+		await this.UserModel.findByIdAndUpdate(_id, {
+			favorites: favorites.includes(movieId)
+				? favorites.filter((id) => String(id) !== String(movieId))
+				: [...favorites, movieId],
+		})
+	}
+
+	async getFavoriteMovies(_id: Types.ObjectId) {
+		return this.UserModel.findById(_id, 'favorites')
+			.populate({
+				path: 'favorites',
+				populate: {
+					path: 'genres',
+				},
+			})
+			.exec()
+			.then((data) => {
+				return data.favorites
+			})
 	}
 }
